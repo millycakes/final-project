59275c13d7d3a00b607ebb49e3bc38f6fa2c91b3
jenna5376 <jenna5376@gmail.com>
2023-12-04 19:30:01 -0500

    updated first screen

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -6,11 +6,11 @@ export default function Index() {
 
   if (Platform.OS === "ios") {
     setTimeout(() => {
-      router.replace("/camera");
+      router.replace("/welcome");
     }, 1)
   } else {
     setImmediate(() => {
-      router.replace("/camera");
+      router.replace("/welcome");
     });
   }
 }
